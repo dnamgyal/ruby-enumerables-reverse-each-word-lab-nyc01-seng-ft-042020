a0cfea068bea7020060a18bff2101edbd521edf6
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-08 00:25:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(word)
   word_array = word.split(' ')
-  
-  word_array.each { |x| x.reverse() }
-  p word_array.join(' ')
+  word_array.collect { |x| x.reverse }.join(' ')
 end
+
